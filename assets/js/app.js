149b18b8f383bd7f597a1699c0d20a851879ee7c
Travis Collins <trc229@gmail.com>
2018-07-04 15:06:55 -0400

    Implement add/delete user from Firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,65 @@
  // Initialize Firebase
- var config = {
-    apiKey: "AIzaSyAdx3teMsjtztrd75depLQ7QbINfghqqO4",
-    authDomain: "rock-paper-scissors-dd84c.firebaseapp.com",
-    databaseURL: "https://rock-paper-scissors-dd84c.firebaseio.com",
-    projectId: "rock-paper-scissors-dd84c",
-    storageBucket: "rock-paper-scissors-dd84c.appspot.com",
-    messagingSenderId: "479130740291"
-  };
-  firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyDmVphIEhlwnC3eNLjADizUs_-22rPhFRE",
+    authDomain: "p5fb-286d4.firebaseapp.com",
+    projectId: "p5fb-286d4",
+};
+firebase.initializeApp(config);
 
- window.onload = function () {
+var userId = null;
+var db = firebase.firestore();
+var settings = {timestampsInSnapshots: true};
+db.settings(settings);
 
-     $('#submit-btn').on('click', function (evt) {
-         if ($('#uname').val()) {
-            var username = $('#uname').val();
-             console.log(username);
-         } else {
-             $('.invalid-feedback').text("Name required");
-         }
-     })
+
+window.onload = function () {
+
+    function addUser(user) {
+        var docRefId = null;
+        if ($('#uname').val()) {
+            docRefId = db.collection('users').add({
+                name: user,
+                score: 0
+            }).then(function(docRef) {
+                return docRef.id;
+            }).catch(function(error) {
+                console.error('Error adding document: ', error);
+            });
+        } else {
+            $('.invalid-feedback').text('Name required');
+        }
+
+        if (docRefId) {
+            return docRefId;
+        }
+    }
+
+    function deleteUser(user) {
+        db.collection('users').doc(user).delete().then(function() {
+            console.log(user + ' successfully deleted');
+        }).catch(function(error) {
+            console.error('Error removing document: ', error);
+        });
+    }
+
+    $('#leet-btn').on('click', function () {
+        if ($('body').css('font-family') === 'LeetSpeak') {
+            $('body').css('font-family', '"Helvetica Neue", Helvetica, Arial, sans-serif');
+            $('#leet-btn').text('Releet');
+        } else {
+            $('body').css('font-family', 'LeetSpeak');
+            $('#leet-btn').text('Deleet');
+        }
+    });
+
+    $('#submit-btn').on('click', function (evt) {
+        var userName = $('#uname').val();
+        userId = addUser(userName);
+    })
+
+    $('#delete-btn').on('click', function () {
+        userId.then(function(id) {
+            deleteUser(id);
+        });
+    });
  } 
