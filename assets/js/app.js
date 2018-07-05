b10129f76835bbcf4e533b03d15e27f08d3099e7
Travis Collins <trc229@gmail.com>
2018-07-05 07:57:58 -0400

    Remove user from db on refresh and closing window

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,47 +2,76 @@
 var config = {
     apiKey: "AIzaSyDmVphIEhlwnC3eNLjADizUs_-22rPhFRE",
     authDomain: "p5fb-286d4.firebaseapp.com",
+    databaseURL: "https://p5fb-286d4.firebaseio.com",
     projectId: "p5fb-286d4",
+    storageBucket: "p5fb-286d4.appspot.com",
+    messagingSenderId: "47868642986"
 };
 firebase.initializeApp(config);
 
-var userId = null;
-var db = firebase.firestore();
+var fs = firebase.firestore();
+var db = firebase.database();
 var settings = {timestampsInSnapshots: true};
-db.settings(settings);
+fs.settings(settings);
+var uid;
 
+window.onload = function() {
+    firebase.auth().signInAnonymously().catch(function(error) {
+        console.error(error.code + ': ' + error.message);
+    });
 
-window.onload = function () {
+    firebase.auth().onAuthStateChanged(function(user) {
+        var user = firebase.auth().currentUser;
+
+        if (user) {
+          // User is signed in.
+          uid = user.uid;
+          console.log(uid);
+        } else {
+          // User is signed out.
+          console.log('User is signed out');
+        }
+      });
 
     function addUser(user) {
-        var docRefId = null;
-        if ($('#uname').val()) {
-            docRefId = db.collection('users').add({
-                name: user,
-                score: 0
-            }).then(function(docRef) {
-                return docRef.id;
-            }).catch(function(error) {
-                console.error('Error adding document: ', error);
-            });
-        } else {
-            $('.invalid-feedback').text('Name required');
-        }
-
-        if (docRefId) {
-            return docRefId;
-        }
+        fs.collection('users').doc(uid).set({
+            connected: true,
+            hasOpponent: false,
+            name: user,
+            score: 0,
+        }).then(function() {
+            console.log('Document Successfully Written');
+        }).catch(function(error) {
+            console.error('Error adding document: ', error);
+        });
     }
 
     function deleteUser(user) {
-        db.collection('users').doc(user).delete().then(function() {
+        fs.collection('users').doc(user).delete().then(function() {
             console.log(user + ' successfully deleted');
         }).catch(function(error) {
             console.error('Error removing document: ', error);
         });
     }
 
-    $('#leet-btn').on('click', function () {
+    function updatePresence(user, isConnected) {
+        var userRef = fs.collection("users").doc(user);
+        
+        return userRef.update({
+            connected: isConnected
+        }).then(function() {
+            console.log(user + ' successfully updated');
+        }).catch(function (error) {
+            console.error('Error updating document: ', error);
+        });
+    }
+
+    function playGame() {
+        $('.login').hide();
+        $('.game').show();
+    }
+
+    $('#leet-btn').on('click', function(e) {
         if ($('body').css('font-family') === 'LeetSpeak') {
             $('body').css('font-family', '"Helvetica Neue", Helvetica, Arial, sans-serif');
             $('#leet-btn').text('Releet');
@@ -52,14 +81,21 @@ window.onload = function () {
         }
     });
 
-    $('#submit-btn').on('click', function (evt) {
+    $('#submit-btn').on('click', function(e) {
         var userName = $('#uname').val();
-        userId = addUser(userName);
+        if (userName) {
+            addUser(userName);
+            playGame();
+        } else {
+            $('.invalid-feedback').text('Name required');
+        }
     })
 
-    $('#delete-btn').on('click', function () {
-        userId.then(function(id) {
-            deleteUser(id);
-        });
-    });
- } 
+    window.onbeforeunload = function () {
+        deleteUser(uid);
+    }
+
+    window.unload = function () {
+        deleteUser(uid);
+    }
+ };
